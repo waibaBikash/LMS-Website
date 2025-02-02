33a6629a37c10af8c2976341c2501428c31be265
waibaBikash <kavrelibikash@gmail.com>
2025-02-02 21:19:02 +1100

    create a pages

---
+++ client/src/pages/educator/Educator.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Educator = () => {
+  return (
+    <div>
+      <h1>Educator page</h1>
+    </div>
+  )
+}
+
+export default Educator
---
+++ client/src/pages/student/CourseList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CourseList = () => {
+  return (
+    <div>
+      <h1>Courses List Page</h1>
+    </div>
+  )
+}
+
+export default CourseList
