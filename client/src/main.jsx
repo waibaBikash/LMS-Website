cd31da24e2c73f66368ae9e436ad7df24e17efd5
waibaBikash <kavrelibikash@gmail.com>
2025-02-02 21:24:54 +1100

    create appcontext

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AppContextProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AppContextProvider>
     <App />
-  </StrictMode>,
+  </AppContextProvider>,
 )
