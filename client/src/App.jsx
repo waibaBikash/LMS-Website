450e34aefe30477e59ebc157320af427a95e03e9
waibaBikash <kavrelibikash@gmail.com>
2025-02-05 21:57:49 +1100

    route page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/student/Home'
+import CourseList from './pages/student/CourseList'
+import CourseDetails from './pages/student/CourseDetails'
+import MyEnrollments from './pages/student/MyEnrollments'
+import Player from './pages/student/Player'
+import Loading from './components/student/Loading'
 
 const App = () => {
   return (
     <div>
        <Routes>
          <Route path='/' element={<Home />} />
+         <Route path='/course-list' element={<CourseList/>} />
+         <Route path='/course-list/:input' element={<CourseList/>} />
+         <Route path='/course/:id' element={<CourseDetails/>} />
+         <Route path='/my-enrollments' element={<MyEnrollments/>} />
+         <Route path='/player/:courseId' element={<Player/>} />
+         <Route path='/loading/:path' element={<Loading/>} />
        </Routes>
     </div>
   )
