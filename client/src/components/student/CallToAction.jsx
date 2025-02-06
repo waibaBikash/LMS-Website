d153350bc19a18f9fdc9ae3265e595eda7a3c783
waibaBikash <kavrelibikash@gmail.com>
2025-02-06 21:23:19 +1100

    save change

--- client/src/components/student/CallToAction.jsx
+++ client/src/components/student/CallToAction.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const CallToAction = () => {
   return (
@@ -8,4 +8,4 @@ const CallToAction = () => {
   )
 }
 
-export default CallToAction
+export default CallToAction;
