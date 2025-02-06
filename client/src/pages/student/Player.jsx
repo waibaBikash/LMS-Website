d55ab4e27a81b8c75268c905f0a611590d465108
waibaBikash <kavrelibikash@gmail.com>
2025-02-06 21:24:29 +1100

    save change

--- client/src/pages/student/Player.jsx
+++ client/src/pages/student/Player.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Player = () => {
   return (
@@ -8,4 +8,4 @@ const Player = () => {
   )
 }
 
-export default Player
+export default Player;
