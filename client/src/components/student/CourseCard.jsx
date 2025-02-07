e3f1f94692714924a43722d03104f4d290906831
waibaBikash <kavrelibikash@gmail.com>
2025-02-07 22:48:13 +1100

    save change

--- client/src/components/student/CourseCard.jsx
+++ client/src/components/student/CourseCard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const CourseCard = () => {
   return (
@@ -8,4 +8,4 @@ const CourseCard = () => {
   )
 }
 
-export default CourseCard
+export default CourseCard;
