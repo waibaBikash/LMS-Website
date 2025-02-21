1c031b6fd409b5a92e630140f968ebcf5896a1b6
waibaBikash <kavrelibikash@gmail.com>
2025-02-21 20:47:23 +1100

    save change

--- client/src/pages/educator/MyCourses.jsx
+++ client/src/pages/educator/MyCourses.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const MyCourses = () => {
   return (
@@ -8,4 +8,4 @@ const MyCourses = () => {
   )
 }
 
-export default MyCourses
+export default MyCourses;
