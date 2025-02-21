110171e0070e0f6f8fdf91485d3d22eff357ece7
waibaBikash <kavrelibikash@gmail.com>
2025-02-21 20:47:02 +1100

    commit change

--- client/src/pages/educator/Educator.jsx
+++ client/src/pages/educator/Educator.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Educator = () => {
   return (
@@ -8,4 +8,4 @@ const Educator = () => {
   )
 }
 
-export default Educator
+export default Educator;
