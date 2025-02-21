d446b81e4db5530fee1a94d1ee65d7472b4e66cc
waibaBikash <kavrelibikash@gmail.com>
2025-02-21 20:45:44 +1100

    save change

--- client/src/pages/educator/Dashboard.jsx
+++ client/src/pages/educator/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Dashboard = () => {
   return (
@@ -8,4 +8,4 @@ const Dashboard = () => {
   )
 }
 
-export default Dashboard
+export default Dashboard;
