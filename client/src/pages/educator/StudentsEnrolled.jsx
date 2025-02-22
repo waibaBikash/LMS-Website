8222d6dd252323c128ca6129bf85cb92e2ffdec5
waibaBikash <kavrelibikash@gmail.com>
2025-02-22 21:47:54 +1100

    save change

--- client/src/pages/educator/StudentsEnrolled.jsx
+++ client/src/pages/educator/StudentsEnrolled.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const StudentsEnrolled = () => {
   return (
@@ -8,4 +8,4 @@ const StudentsEnrolled = () => {
   )
 }
 
-export default StudentsEnrolled
+export default StudentsEnrolled;
