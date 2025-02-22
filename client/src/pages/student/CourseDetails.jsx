c90ec990f8c4c85b875ee63a37a7957b12fde4c8
waibaBikash <kavrelibikash@gmail.com>
2025-02-22 21:48:20 +1100

    commit change

--- client/src/pages/student/CourseDetails.jsx
+++ client/src/pages/student/CourseDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const CourseDetails = () => {
   return (
@@ -8,4 +8,4 @@ const CourseDetails = () => {
   )
 }
 
-export default CourseDetails
+export default CourseDetails;
