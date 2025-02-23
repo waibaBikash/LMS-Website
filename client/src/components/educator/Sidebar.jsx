96ee508fb05512d614774a252fd2dff7bbdaf1f4
waibaBikash <kavrelibikash@gmail.com>
2025-02-23 22:13:25 +1100

    save change

--- client/src/components/educator/Sidebar.jsx
+++ client/src/components/educator/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Sidebar = () => {
   return (
@@ -8,4 +8,4 @@ const Sidebar = () => {
   )
 }
 
-export default Sidebar
+export default Sidebar;
